9d8231db5b067d2c529821c691aa2660b2fd76ae
Emanuel <emanuelatencia2407@gmail.com>
2026-03-30 11:21:38 -0500

    Crear método para validar stock de una lista de productos

--- src/main/java/com/bazar/apibazar/service/ProductoService.java
+++ src/main/java/com/bazar/apibazar/service/ProductoService.java
@@ -1,7 +1,9 @@
 package com.bazar.apibazar.service;
 
 import com.bazar.apibazar.dto.ProductoDto;
+import com.bazar.apibazar.dto.VentaProductoDto;
 import com.bazar.apibazar.exception.ProductoNotFoundException;
+import com.bazar.apibazar.exception.ProductoStockInsuficienteException;
 import com.bazar.apibazar.model.Producto;
 import com.bazar.apibazar.repository.IProductoRepository;
 import java.util.ArrayList;
@@ -13,10 +15,26 @@ import org.springframework.stereotype.Service;
 @Service
 public class ProductoService implements IProductoService{
 
-    //Inyección de dependecia para ProductoRepository
+    //Inyección de dependencia para ProductoRepository
     @Autowired
     private IProductoRepository productoRepository;
 
+    //Método propio para validar si el stock de una lista de productos es suficiente para cubrir una cierta cantidad
+    public void validarStockProductos(List<VentaProductoDto> productosValidarStock){
+
+        //Recorremos la lista para validar uno por uno los productos
+        for(VentaProductoDto productoValidar: productosValidarStock){
+            //Buscamos producto, si no existe -> Excepción de dominio
+            Producto objProducto = findProducto(productoValidar.getProductoId());
+
+            //Si el stock es insuficiente -> Excepción de dominio
+            if(objProducto.getCantidadDisponible() < productoValidar.getCantidad()){
+                throw new ProductoStockInsuficienteException(
+                    "El producto con id: " + productoValidar.getProductoId() + " no tiene stock suficiente para cubrir la cantidad: " + productoValidar.getCantidad()
+                );
+            }
+        } //Si llega el final del bucle y no hay excepciones -> Los productos son válidos para vender
+    }
 
     @Override
     public List<Producto> getProductos() {
@@ -48,7 +66,7 @@ public class ProductoService implements IProductoService{
 
     }
 
-    //Sobrecarg del método saveProducto que será útil en venta-service para registrar un producto desde allá
+    //Sobre-carga del método saveProducto que será útil en venta-service para registrar un producto desde allá
     public void saveProducto(Producto objNuevo) {productoRepository.save(objNuevo);}
 
     //Método usado por venta-service para registrar una lista de productos
