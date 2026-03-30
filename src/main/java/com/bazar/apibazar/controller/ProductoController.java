989da45cb72fa377775277292768825b079e9b4d
Emanuel <emanuelatencia2407@gmail.com>
2026-03-29 20:25:31 -0500

    Usar ResponseEntity en todos los métodos de productoController

--- src/main/java/com/bazar/apibazar/controller/ProductoController.java
+++ src/main/java/com/bazar/apibazar/controller/ProductoController.java
@@ -5,6 +5,7 @@ import com.bazar.apibazar.model.Producto;
 import com.bazar.apibazar.service.IProductoService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,7 +15,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -27,44 +27,46 @@ public class ProductoController {
 
     //Traer todos
     @GetMapping("/")
-    @ResponseBody
-    public List<Producto> getProductos(){
-        return productoService.getProductos();
+    public ResponseEntity<List<Producto>> getProductos(){
+        return ResponseEntity.ok(productoService.getProductos());
     }
 
     //Traer uno
     @GetMapping("/{id}")
-    public ResponseEntity<?> findProducto(@PathVariable Long id){
+    public ResponseEntity<Producto> findProducto(@PathVariable Long id){
         return ResponseEntity.ok(productoService.findProducto(id));
 
     }
 
     //Productos con stock < 5
     @GetMapping("/falta-stock")
-    public List<Producto> productosPocoStock(){
-        return productoService.productosPocoStock();
+    public ResponseEntity<List<Producto>> productosPocoStock(){
+        return ResponseEntity.ok(productoService.productosPocoStock());
     }
 
-    //Ingresamos
+    //Ingresamos producto nuevo
     @PostMapping("/")
-    public void saveProducto(@RequestBody ProductoDto objNuevo){productoService.saveProducto(objNuevo);}
+    public ResponseEntity<Producto> saveProducto(@RequestBody ProductoDto objNuevo){
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(productoService.saveProducto(objNuevo));
+    }
 
-    //Eliminamos
+    //Eliminamos producto por id
     @DeleteMapping("/{id}")
-    public ResponseEntity<?> deleteProducto(@PathVariable Long id){
+    public ResponseEntity<Void> deleteProducto(@PathVariable Long id){
         productoService.deleteProducto(id);
         return ResponseEntity.noContent().build();
     }
 
     //Actualizamos
     @PutMapping("/{id}")
-    public ResponseEntity<?> updateProducto(@PathVariable Long id, @RequestBody ProductoDto objActualizado){
+    public ResponseEntity<Producto> updateProducto(@PathVariable Long id, @RequestBody ProductoDto objActualizado){
         return ResponseEntity.ok(productoService.updateProducto(id, objActualizado));
     }
 
     //Actualización parcial
     @PatchMapping("/{id}")
-    public ResponseEntity<?> patchProducto(@PathVariable Long id, @RequestBody ProductoDto objDto){
+    public ResponseEntity<Producto> patchProducto(@PathVariable Long id, @RequestBody ProductoDto objDto){
         return ResponseEntity.ok(productoService.patchProducto(id, objDto));
     }
 
