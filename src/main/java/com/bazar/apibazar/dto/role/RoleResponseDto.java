cbf31aaeeab1fd4086ec5f625d4c3c7afe7c0ee3
Emanuel <emanuelatencia2407@gmail.com>
2026-05-07 16:43:55 -0500

    Corregir tipo de dato en el id del rol: String -> Long

--- src/main/java/com/bazar/apibazar/dto/role/RoleResponseDto.java
+++ src/main/java/com/bazar/apibazar/dto/role/RoleResponseDto.java
@@ -5,5 +5,5 @@ import com.bazar.apibazar.dto.permission.PermissionResponseDto;
 import java.util.List;
 
 //DTO para transferir los datos de un role al cliente
-public record RoleResponseDto(String id, String roleName, List<PermissionResponseDto> permissions) {
+public record RoleResponseDto(Long id, String roleName, List<PermissionResponseDto> permissions) {
 }
