20a316d932b83db7ead0cad75afd86d04f59929c
Emanuel <emanuelatencia2407@gmail.com>
2026-05-07 11:05:05 -0500

    Crear entidad encargada del registro de usuarios dentro la aplicación

---
+++ src/main/java/com/bazar/apibazar/model/UserSec.java
@@ -0,0 +1,42 @@
+package com.bazar.apibazar.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+//Entidad para el registro de usuarios
+@Getter  @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "users")
+@Entity
+public class UserSec {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    //No debe haber dos usuarios con el mismo username
+    @Column(unique = true)
+    private String username;
+    private String password;
+    //Columnas para gestionar el estado del usuario (Al momento de la creación todas serán positivas)
+    private boolean enabled=true;  //Si no ha sido eliminado o deshabilitado
+    private boolean accountNotExpired=true; //Si la cuenta no ha expirado
+    private boolean accountNotLocked=true;  //Si la cuenta no está bloqueada
+    private boolean credentialNotExpired=true;  //Si las credenciales no han expirado
+    //Relación ManyToMany con tipo de carga ansiosa (Cuando se carga un usuario, se cargan sus roles)
+    @ManyToMany(fetch = FetchType.EAGER)
+    //Definimos propiedades de tabla intermedia
+    //joinColumns() -> FK que hace referencia al lado dueño de la relación (UserSec)
+    //inverseJoinColums() -> FK que hace referencia al otro lado, o lado inverso de la relación (Role)
+    @JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    Set<Role> listRoles = new LinkedHashSet<>();
+
+
+}
