e5bef44ef385aa24352abe9caf4b9a1b9d95e2e7
Emanuel <emanuelatencia2407@gmail.com>
2026-05-07 18:21:17 -0500

    Crear implementación de interfaz IRoleService

---
+++ src/main/java/com/bazar/apibazar/service/RoleService.java
@@ -0,0 +1,156 @@
+package com.bazar.apibazar.service;
+
+import com.bazar.apibazar.dto.role.RoleRequestDto;
+import com.bazar.apibazar.dto.role.RoleResponseDto;
+import com.bazar.apibazar.exception.PermissionNotFoundException;
+import com.bazar.apibazar.exception.RoleNotFoundException;
+import com.bazar.apibazar.model.Role;
+import com.bazar.apibazar.repository.IRoleRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.List;
+
+//Service para desarrollar toda la lógica de negocio que respecta al dominio <<Role>>
+@Service
+public class RoleService implements IRoleService{
+
+    //Inyección de dependencia para el repositorio de persistencia de <<Role>>
+    private final IRoleRepository roleRepo;
+    //Inyección de dependencia para PermissionService
+    private final PermissionService permissionService;
+    //Inyección de dependencia por constructor
+    public RoleService(IRoleRepository roleRepo, PermissionService permissionService){
+        this.roleRepo = roleRepo;
+        this.permissionService = permissionService;
+    }
+
+    //Método para construir, a partir de los datos de un <<Role>>, un DTO para exponer ese <<Role>>
+    private RoleResponseDto buildRoleResponse(Role objRole){
+        //Sacamos instancia de RoleResponseDto
+        return new RoleResponseDto(
+                objRole.getId(), objRole.getName(),
+                    /*Como los permisos dentro de este DTO también son objetos DTO, debemos llamar al service de Permission
+                       para construir la lista de DTO a partir de los datos de los permisos que están dentro del rol*/
+                    permissionService.buildPermissionsResponse(
+                            new ArrayList<>(objRole.getListPermissions())
+                    )
+        );
+    }
+
+    //Método para construir una lista de objetos DTO de respuesta a partir de los datos de una lista de <<Roles>>
+    public List<RoleResponseDto> buildRolesResponse(List<Role> listRoles){
+        //Lista de objetos DTO a exponer
+        List<RoleResponseDto> rolesExponer = new ArrayList<>();
+
+        //Recorremos la lista con el método .forEach()
+        listRoles.forEach(
+                //Usamos una función lambda que construya cada DTO a partir de los roles de la lista "listRoles" y lo agregue a "rolesExponer"
+                role -> rolesExponer.add(buildRoleResponse(role))
+        );
+
+        return rolesExponer;
+    }
+
+    //Método para consultar los datos de un role por su id y en caso de no encontrarlo lanzar excepción personalizada
+    private Role findRole(Long id){
+        return roleRepo.findById(id)
+                .orElseThrow(
+                        //Creamos función lambda para retornar la excepción en caso de que no se encuentre el rol
+                        () -> new RoleNotFoundException("No se encontró role con Id: + id")
+                );
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public List<RoleResponseDto> findAllRole() {
+        return buildRolesResponse(roleRepo.findAll());
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public RoleResponseDto findRoleById(Long id) {
+        //Buscamos al rol por su ID
+        Role objRole = findRole(id);
+
+        //Exponemos al cliente por medio de DTO
+        return buildRoleResponse(objRole);
+    }
+
+    @Transactional
+    @Override
+    public RoleResponseDto saveRole(RoleRequestDto newRole) {
+        //Construimos el objeto del rol que se va a persistir
+        Role objRole = new Role(null, newRole.roleName(),
+                new LinkedHashSet<>(
+                        /*Como el DTO "RoleRequestDto" tiene dentro una lista de ids de permisos que le vamos a asignar a
+                          este rol, debemos buscar esos permisos*/
+                        permissionService.findAllRolesById(newRole.permissionsIds()))
+        );
+
+        //Persistimos objeto
+        Role persistedRole = roleRepo.save(objRole);
+
+        return buildRoleResponse(persistedRole);
+    }
+
+    @Transactional
+    @Override
+    public void deleteRol(Long id) {
+        //Buscamos rol para verificar existencia
+        Role objRole = findRole(id);
+
+        roleRepo.delete(objRole);
+    }
+
+    @Transactional
+    @Override
+    public RoleResponseDto updateRole(Long idRole, RoleRequestDto updatedRol) {
+        //Buscamos rol para verificar existencia
+        Role objRole = findRole(idRole);
+
+        //Si se quiere actualizar el nombre del rol, se actualiza
+        if(updatedRol.roleName() != null){objRole.setName(updatedRol.roleName());}
+
+        //Construimos DTO a partir del rol actualizado
+        return buildRoleResponse(objRole);
+    }
+
+    @Transactional
+    @Override
+    public RoleResponseDto addPermissionsToRole(Long idRole, List<Long> newPermissionsIds) {
+        //Buscamos rol para confirmar existencia
+        Role objRole = findRole(idRole);
+
+        //Buscamos y agregamos nueva lista de permisos al rol
+        objRole.getListPermissions().addAll(
+                permissionService.findAllRolesById(newPermissionsIds)
+        );
+
+        //Exponemos rol actualizado
+        return buildRoleResponse(objRole);
+    }
+
+    @Transactional
+    @Override
+    public RoleResponseDto removePermissionFromRole(Long idRole, Long idPermissionEliminar) {
+        //Buscamos rol para confirmar existencia
+        Role objRole = findRole(idRole);
+
+        //Buscamos permission para confirmar existencia
+        permissionService.findPermissionById(idPermissionEliminar);
+
+        //Usamos el método .removeIf() y una función lambda para eliminar todo permiso que cumpla la condición definida
+        //.removeIf() devuelve un valor booleano indicando si se eliminó algo o no
+        boolean removed = objRole.getListPermissions().removeIf(
+                permission -> permission.getId().equals(idPermissionEliminar)
+        ) ;
+
+        //Si no existe el permiso dentro del rol -> Lo indicamos con excepción personalizada
+        if(!removed){throw new PermissionNotFoundException("No existe permiso dentro del rol con id: " + idPermissionEliminar);}
+
+        return buildRoleResponse(objRole);
+    }
+}
