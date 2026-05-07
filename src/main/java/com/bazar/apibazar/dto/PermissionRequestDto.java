98869b293b0d018b7d30ce0b90a5ea54e2d80655
Emanuel <emanuelatencia2407@gmail.com>
2026-05-07 11:15:03 -0500

    Crear DTOs para la tranferencia de datos de los permisos cuando se registran o se exponen al cliente

---
+++ src/main/java/com/bazar/apibazar/dto/PermissionRequestDto.java
@@ -0,0 +1,5 @@
+package com.bazar.apibazar.dto;
+
+//DTO para transferir los datos de un permiso que se desea registrar
+public record PermissionRequestDto(String permissionName) {
+}
---
+++ src/main/java/com/bazar/apibazar/dto/PermissionResponseDto.java
@@ -0,0 +1,5 @@
+package com.bazar.apibazar.dto;
+
+//DTO para exponer los datos de un permiso
+public record PermissionResponseDto(Long id, String permissionName) {
+}
