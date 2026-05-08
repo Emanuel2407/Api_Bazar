e1a954faf32323f0954cacdb17e0143242dee7e3
Emanuel <emanuelatencia2407@gmail.com>
2026-05-07 22:29:10 -0500

    Crear excepción personalizada para indicar que un username ya esta en uso

--- src/main/java/com/bazar/apibazar/exception/UsernameAlreadyExistsException.java
+++ src/main/java/com/bazar/apibazar/exception/UsernameAlreadyExistsException.java
@@ -1,8 +1,8 @@
 package com.bazar.apibazar.exception;
 
 //Excepción de infraestructura de seguridad para indicar que un username ya está registrado
-public class UsernameIAlreadyExistsException extends RuntimeException {
-    public UsernameIAlreadyExistsException(String message) {
+public class UsernameAlreadyExistsException extends RuntimeException {
+    public UsernameAlreadyExistsException(String message) {
         super(message);
     }
 }
