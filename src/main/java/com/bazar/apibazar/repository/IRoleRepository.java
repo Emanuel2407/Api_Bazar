0df79e3ae88a3afeb6c1f71d05f2e586f6c8b83d
Emanuel <emanuelatencia2407@gmail.com>
2026-05-08 19:23:29 -0500

    Definir método para traer de la bd una lista de roles que tienen asignado un permiso en particular

--- src/main/java/com/bazar/apibazar/repository/IRoleRepository.java
+++ src/main/java/com/bazar/apibazar/repository/IRoleRepository.java
@@ -4,7 +4,13 @@ import com.bazar.apibazar.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 //Definimos repositorio de la entidad Role que hereda métodos de la interfaz JpaRepository para la persistencia y gestión de roles
 @Repository
 public interface IRoleRepository extends JpaRepository<Role, Long> {
+
+    //Definimos método que Spring-Data-Jpa interpreta para traer una lista de roles que tengan un permiso cuyo id sea el mandado por parámetro
+    List<Role> findByListPermissions_id(Long permissionId);
 }
