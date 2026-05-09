aa727c851cf186934e54758d8558ba85bc66aec3
Emanuel <emanuelatencia2407@gmail.com>
2026-05-09 10:04:14 -0500

    Implementar métodos Equals/Hashcode para localizar un permiso de forma correcta en el contenxto de persistencia

--- src/main/java/com/bazar/apibazar/model/Permission.java
+++ src/main/java/com/bazar/apibazar/model/Permission.java
@@ -1,16 +1,16 @@
 package com.bazar.apibazar.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 //Entidad encargada del registro de las acciones que los usuarios tienen permitido hacer dentro de la app
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+// Usamos solo el ID para equals/hashCode. Esto garantiza que JPA y las Colecciones (como el Set en Role)
+// identifiquen correctamente la entidad por su valor en BD y no por su instancia en memoria.
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 //Parámetros de la tabla: name=permissions
 @Table(name = "permissions")
 @Entity
@@ -18,6 +18,7 @@ public class Permission {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
     private Long id;
     //No tiene sentido tener un permiso repetido
     @Column(unique = true)
