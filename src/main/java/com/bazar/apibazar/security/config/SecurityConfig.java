dd63fff99707790507eaba2d1c7ac337c97a4718
Emanuel <emanuelatencia2407@gmail.com>
2026-05-09 15:59:34 -0500

    Definir reglas con base a las cuales se creará la SecurityFilterChain

--- src/main/java/com/bazar/apibazar/security/config/SecurityConfig.java
+++ src/main/java/com/bazar/apibazar/security/config/SecurityConfig.java
@@ -14,4 +14,20 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean  //Bean que registra la cadena de filtros de seguridad que interceptan cada request
+    public SecurityFilterChain securityFilter(HttpSecurity httpSecurity) throws Exception {
+        return  httpSecurity
+                // Se desactiva CSRF (común en APIs REST que no usan cookies/sesiones)
+                .csrf(csrf -> csrf.disable())
+                // Configurar la gestión de sesiones como Stateless
+                .sessionManagement(sesion -> sesion.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                //Autorización de peticiones
+                .authorizeHttpRequests(auth -> {
+                    //Inicialmente, se define que todas las request serán públicas
+                    auth.anyRequest().permitAll();
+                })
+                //Construcción de la cadena de filtros de seguridad con base a las reglas anteriores
+                .build();
+    }
 }
