ab33bfd89cccad85bfd64fc95b0cbab03d1506c4
Emanuel <emanuelatencia2407@gmail.com>
2026-05-10 16:06:36 -0500

    Cambiar tipo de dato en campo authorities

--- src/main/java/com/bazar/apibazar/dto/user/UserLoginResponseDto.java
+++ src/main/java/com/bazar/apibazar/dto/user/UserLoginResponseDto.java
@@ -1,5 +1,7 @@
 package com.bazar.apibazar.dto.user;
 
+import java.util.List;
+
 //Dto para transferir los datos de un usuario y el token JWT que se genera cuando este hace login
-public record UserLoginResponseDto(String username, String token, String authorities) {
+public record UserLoginResponseDto(String username, String token, List<String> authorities) {
 }
