c98fd798572ec04130c9c48e965b2649ee134ef4
Emanuel <emanuelatencia2407@gmail.com>
2026-05-14 09:13:36 -0500

    Cambiar tipo de relación con cliente a unidireccional del lado de venta

--- src/main/java/com/bazar/apibazar/model/Venta.java
+++ src/main/java/com/bazar/apibazar/model/Venta.java
@@ -1,12 +1,7 @@
 package com.bazar.apibazar.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.OneToOne;
+import jakarta.persistence.*;
+
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
@@ -23,7 +18,16 @@ public class Venta {
     private LocalDate fechaVenta;
     private Double totalVenta;
     private Integer cantidadTotalProductos;
-    
+
+    //Referencia al cliente dueño de la venta
+    /*Definimos relación unidireccional one-to-many (many-to-one del lado de venta) con la entidad cliente para que solo la venta
+     conozca el cliente que la realizó, estableciendo a venta como lado dueño*/
+    //FetchType fuerza una carga ansiosa, por lo que cuando se busque a una venta, esta vendrá con el cliente que la realizó
+    @ManyToOne(fetch = FetchType.EAGER)
+    //Definimos nombre de la FK en la table cliente
+    @JoinColumn(name = "cliente_id")
+    private Cliente cliente;
+
     /*Relación n a n con Producto, se implementará por medio de dos relaciones 1 a n, la primera va de Venta a
     la tabla intermedia VentaProducto(relación bidireccional) y la segunda de Producto a la tabla intermedia 
     VentaProducto(relación unidireccional). Debemos usar el mappedBy para hacer la relación bidireccional con 
