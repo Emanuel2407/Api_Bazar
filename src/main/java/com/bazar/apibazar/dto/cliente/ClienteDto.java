bb87220344966f79b07123885632bd8c0ded3930
Emanuel <emanuelatencia2407@gmail.com>
2026-05-15 17:06:19 -0500

    Agregar annotations para validar propiedades al registrar un cliente

--- src/main/java/com/bazar/apibazar/dto/cliente/ClienteDto.java
+++ src/main/java/com/bazar/apibazar/dto/cliente/ClienteDto.java
@@ -1,7 +1,9 @@
 package com.bazar.apibazar.dto.cliente;
 
 
+import jakarta.validation.constraints.NotBlank;
+
 //DTO para recibir los datos de un cliente cuando se va a hacer un registro o una modificación de este
-public record ClienteDto(String nombre,
-                         String apellido,
-                         String documento){}
+public record ClienteDto(@NotBlank String nombre,
+                         @NotBlank String apellido,
+                         @NotBlank String documento){}
