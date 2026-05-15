f2e5bfaaa03eefdc70159bbc7d8a08a0dddd9b31
Emanuel <emanuelatencia2407@gmail.com>
2026-05-15 11:52:43 -0500

    Definir método para consultar una lista de roles por sus nombres

--- src/main/java/com/bazar/apibazar/repository/IRoleRepository.java
+++ src/main/java/com/bazar/apibazar/repository/IRoleRepository.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 //Definimos repositorio de la entidad Role que hereda métodos de la interfaz JpaRepository para la persistencia y gestión de roles
 @Repository
@@ -13,4 +14,7 @@ public interface IRoleRepository extends JpaRepository<Role, Long> {
 
     //Definimos método que Spring-Data-Jpa interpreta para traer una lista de roles que tengan un permiso cuyo id sea el mandado por parámetro
     List<Role> findByListPermissions_id(Long permissionId);
+
+    //Definimos método de consulta personalizado para consultar los datos de un grupo de roles consultando por sus nombres
+    List<Role> findAllByNameIn(Set<String> rolesNames);
 }
