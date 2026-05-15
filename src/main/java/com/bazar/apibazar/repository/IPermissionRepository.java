4dcfc9b043c16e13116fb17eaee2600a2678fce0
Emanuel <emanuelatencia2407@gmail.com>
2026-05-15 12:25:11 -0500

    Definir método para consultar una lista de permisos por sus nombres

--- src/main/java/com/bazar/apibazar/repository/IPermissionRepository.java
+++ src/main/java/com/bazar/apibazar/repository/IPermissionRepository.java
@@ -4,7 +4,13 @@ import com.bazar.apibazar.model.Permission;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Set;
+
 //Definimos contrato con Spring Data Jpa para tener a los diferentes métodos de persistencia de la interfaz JpaRepository<>
 @Repository
 public interface IPermissionRepository extends JpaRepository<Permission, Long> {
+
+    //Definimos método de consulta personalizada que Spring-Data-Jpa interpreta para traer, consultando por sus nombres, una lista de permisos
+    List<Permission> findAllByNameIn(Set<String> permissionsNames);
 }
