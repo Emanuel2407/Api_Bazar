b9e11e4b9cc8b0a15c0f2678c00a2e88301fc49a
Emanuel <emanuelatencia2407@gmail.com>
2026-05-15 18:59:17 -0500

    Crear DTO para transferir los datos del  usuario en un registro público

---
+++ src/main/java/com/bazar/apibazar/dto/user/ClientUserRequestDto.java
@@ -0,0 +1,10 @@
+package com.bazar.apibazar.dto.user;
+
+import com.bazar.apibazar.dto.cliente.ClienteDto;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+//DTO para transferir los datos de los usuarios que se registren por el registro público (clientes)
+public record ClientUserRequestDto(@NotBlank String username, @NotBlank String password, @NotNull @Valid ClienteDto clientData) {
+}
