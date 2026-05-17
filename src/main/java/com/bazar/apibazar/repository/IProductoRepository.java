e822367edec79fe401fdf7550acaa29830bf06c4
Emanuel <emanuelatencia2407@gmail.com>
2026-05-16 19:18:33 -0500

    Definir método para consultar en la bd los productos disponibles para vender

--- src/main/java/com/bazar/apibazar/repository/IProductoRepository.java
+++ src/main/java/com/bazar/apibazar/repository/IProductoRepository.java
@@ -4,7 +4,11 @@ import com.bazar.apibazar.model.Producto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface IProductoRepository extends JpaRepository<Producto, Long>{
-    
+
+    //Consulta personalizada para traer de la base de datos solo los productos que estén disponibles para vender (available=true)
+    List<Producto> findByAvailableTrue();
 }
