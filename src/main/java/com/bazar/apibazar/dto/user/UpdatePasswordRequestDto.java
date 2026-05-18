d08bc025fb50e357640d06592e01e60a116e7175
Emanuel <emanuelatencia2407@gmail.com>
2026-05-18 13:55:09 -0500

    Validar campos obligatorios en DTOs de modificación de datos de usuario

--- src/main/java/com/bazar/apibazar/dto/user/UpdatePasswordRequestDto.java
+++ src/main/java/com/bazar/apibazar/dto/user/UpdatePasswordRequestDto.java
@@ -1,6 +1,9 @@
 package com.bazar.apibazar.dto.user;
 
+import jakarta.validation.constraints.NotBlank;
+
 /*DTO enfocado en el cambio de contraseña de un usuario, este debe cumplir ciertos requisitos para efectuar este cambio
  (Como que debe conocer su contraseña actual para poder modificarla)*/
-public record UpdatePasswordRequestDto(String currentPassword, String newPassword) {
+public record UpdatePasswordRequestDto(@NotBlank String currentPassword,
+                                       @NotBlank String newPassword) {
 }
--- src/main/java/com/bazar/apibazar/dto/user/UpdateUsernameRequestDto.java
+++ src/main/java/com/bazar/apibazar/dto/user/UpdateUsernameRequestDto.java
@@ -1,5 +1,8 @@
 package com.bazar.apibazar.dto.user;
 
+import jakarta.validation.constraints.NotBlank;
+
 //DTO para modificar el username de un usuario (Este debe conocer su contraseña para efectuar el cambio)
-public record UpdateUsernameRequestDto(String password, String newUsername) {
+public record UpdateUsernameRequestDto(@NotBlank String password,
+                                       @NotBlank String newUsername) {
 }
