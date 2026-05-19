3beab5a23d75a44d2b989bf6468e9c90f47f382e
Emanuel <emanuelatencia2407@gmail.com>
2026-05-19 13:08:18 -0500

    Validar que el campo documento sea númerico al registrar o actualizar un cliente

--- src/main/java/com/bazar/apibazar/dto/cliente/ClientePatchDto.java
+++ src/main/java/com/bazar/apibazar/dto/cliente/ClientePatchDto.java
@@ -1,8 +1,9 @@
 package com.bazar.apibazar.dto.cliente;
 
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 
 //DTO para recibir los datos de un cliente cuando se va a hacer una modificación parcial de este
 public record ClientePatchDto(String nombre,
                               String apellido,
-                              String documento){}
+                              //Con @Pattern(regexp = \\d+") aseguramos que el campo documento sea un String numérico
+                              @Pattern(regexp = "\\d+") String documento){}
--- src/main/java/com/bazar/apibazar/dto/cliente/ClienteRequestDto.java
+++ src/main/java/com/bazar/apibazar/dto/cliente/ClienteRequestDto.java
@@ -2,8 +2,10 @@ package com.bazar.apibazar.dto.cliente;
 
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 
 //DTO para recibir los datos de un cliente cuando se va a hacer un registro o una modificación de este
 public record ClienteRequestDto(@NotBlank String nombre,
                                 @NotBlank String apellido,
-                                @NotBlank String documento){}
+                                //Con attern(regexp = "\\d+") aseguramos que el campo sea un String numérico
+                                @NotBlank @Pattern(regexp = "\\d+")String documento){}
