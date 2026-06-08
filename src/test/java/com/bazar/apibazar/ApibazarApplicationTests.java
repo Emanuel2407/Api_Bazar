27b71ffb9f6f94017c9c3e2f53e9fff9eab870b2
Emanuel <emanuelatencia2407@gmail.com>
2026-06-08 10:18:36 -0500

    test: configurar entorno de pruebas y corregir levantamiento del contexto de api-bazar

--- src/test/java/com/bazar/apibazar/ApibazarApplicationTests.java
+++ src/test/java/com/bazar/apibazar/ApibazarApplicationTests.java
@@ -1,9 +1,11 @@
-/*package com.bazar.apibazar;
+package com.bazar.apibazar;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ApibazarApplicationTests {
 
 	@Test
@@ -11,4 +13,3 @@ class ApibazarApplicationTests {
 	}
 
 }
-*/
