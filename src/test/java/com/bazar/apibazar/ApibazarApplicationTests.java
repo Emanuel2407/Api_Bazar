b7db16bf2c9158eefa6a9591985101e9e5c9bba8
Emanuel <emanuelatencia2407@gmail.com>
2026-06-12 20:15:34 -0500

    test: crear mock de bootstrap para ejecución de los tests

--- src/test/java/com/bazar/apibazar/ApibazarApplicationTests.java
+++ src/test/java/com/bazar/apibazar/ApibazarApplicationTests.java
@@ -1,12 +1,17 @@
 package com.bazar.apibazar;
 
+import com.bazar.apibazar.bootstrap.SystemBootstrap;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class ApibazarApplicationTests {
+
+	@MockitoBean
+	private SystemBootstrap systemBootstrap;
 
 	@Test
 	void contextLoads() {
